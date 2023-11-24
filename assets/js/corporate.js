9203f5bc97a2a1bf8f5cb4bfdb9ff27f632cffc1
Nikhil <nikhilgb3113@gmail.com>
2023-11-25 00:14:43 +0530

    first commit

---
+++ assets/js/corporate.js
@@ -0,0 +1,19 @@
+const showBtn = document.querySelector(".btn-bars"),
+  closeBtn = document.querySelector(".btn-close"),
+  navMenu = document.querySelector(".navbar-collapse");
+showBtn.addEventListener("click", () => {
+  navMenu.classList.add("showMenu");
+});
+closeBtn.addEventListener("click", () => {
+  navMenu.classList.remove("showMenu");
+});
+
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  const question = item.querySelector(".faq-question");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
