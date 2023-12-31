b1fb2ddd6a8956fdf6d62856d4cccfac6376488f
Nikhil <nikhilgb3113@gmail.com>
2023-12-31 15:11:07 +0530

    Changed Corporate Section

--- assets/js/corporate.js
+++ assets/js/corporate.js
@@ -39,6 +39,37 @@ function nextStep() {
   }
 }
 
+// slider code
+
+let slideIndex = 0;
+
+function showSlides() {
+  const slides = document.querySelectorAll(".slide");
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+
+  slides[slideIndex].classList.add("active");
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  showSlides();
+}
+
+setInterval(() => {
+  slideIndex++;
+  showSlides();
+}, 3000);
+
+showSlides();
+
 gsap.from(".navbar", {
   y: -50,
   duration: 1,
